01fae62be2a8f07b165b6d5d9be1d11231e26467
Marko Zlatar <markozlatar01@gmail.com>
2020-02-25 11:26:53 +0100

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app :dark="false">
+    
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    search() {
+      console.log('Search works')
+    }
+  }
+}
+</script>
