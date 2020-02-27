fe37b2fdf6210b1c83eacc2eb6c9cd66bc93ea69
Marko Zlatar <markozlatar01@gmail.com>
2020-02-27 12:13:20 +0100

    CREATED: Home feed card

---
+++ components/home/AppFeedCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card class="mb-5">
+    <v-img :aspect-ratio="$vuetify.breakpoint.xsOnly ? 1 : 16/9" src="/feed-img.jpg"></v-img>
+    <v-list-item>
+      <v-list-item-avatar>
+        <v-img src="/avatar.png"></v-img>
+      </v-list-item-avatar>
+      <v-list-item-content>
+        <v-list-item-title class="headline">This is headling</v-list-item-title>
+        <v-list-item-subtitle>by First Last</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+    <v-card-text>This is my super cool text for dummy feed card that i going to see every time when i open this project for long time...</v-card-text>
+    <v-card-actions>
+      <v-btn text>Read more</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -11,27 +11,9 @@
       </v-row>
       <v-row class="align-start justify-center">
         <!-- FEED -->
-        <v-col cols="12" md="8" xl="7">
-          <v-card class="mb-5" v-for="n in 10" :key="n">
-            <v-img :aspect-ratio="$vuetify.breakpoint.xsOnly ? 1 : 16/9" src="/feed-img.jpg"></v-img>
-            <v-list-item>
-              <v-list-item-avatar>
-                <v-img src="/avatar.png"></v-img>
-              </v-list-item-avatar>
-              <v-list-item-content>
-                <v-list-item-title class="headline">This is headling</v-list-item-title>
-                <v-list-item-subtitle>by First Last</v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-            <v-card-text>This is my super cool text for dummy feed card that i going to see every time when i open this project for long time...</v-card-text>
-            <v-card-actions>
-              <v-btn text>Read more</v-btn>
-              <v-spacer></v-spacer>
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-            </v-card-actions>
-          </v-card>
+        <v-progress-circular indeterminate class="my-12" v-if="false"></v-progress-circular>
+        <v-col v-else cols="12" md="8" xl="7">
+          <app-feed-card v-for="n in 10" :key="n"></app-feed-card>
         </v-col>
       </v-row>
     </v-container>
@@ -39,8 +21,12 @@
 </template>
 
 <script>
+import AppFeedCard from '@/components/home/AppFeedCard'
+
 export default {
-  components: {},
+  components: {
+    AppFeedCard
+  },
   methods: {}
 }
 </script>
