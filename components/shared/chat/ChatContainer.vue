4410c66d33fc9e52fa8d8baa80fd869a1d28e0af
Marko Zlatar <markozlatar01@gmail.com>
2020-02-29 20:00:51 +0100

    CREATED: ChatBox and ChatContainer components

---
+++ components/shared/chat/ChatContainer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="chat-container">
+    <chat-box class="mr-2"></chat-box>
+  </div>
+</template>
+
+<script>
+import ChatBox from '@/components/shared/chat/ChatBox'
+
+export default {
+  data() {
+    return {
+      show: true
+    }
+  },
+  components: {
+    ChatBox
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.chat-container {
+  position: fixed;
+  bottom: 36px;
+  left: 8px;
+  display: flex;
+  width: auto;
+  height: auto;
+  max-width: 100%;
+  overflow-x: scroll;
+  z-index: 1000000;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -4,6 +4,7 @@
     <nuxt />
     <the-navigation-drawer-chat></the-navigation-drawer-chat>
     <post-dialog></post-dialog>
+    <chat-container></chat-container>
     <app-footer></app-footer>
   </v-app>
 </template>
@@ -13,13 +14,15 @@ import TheNavbar from '@/components/shared/navbar/TheNavbar'
 import AppFooter from '@/components/shared/footer/AppFooter'
 import TheNavigationDrawerChat from '@/components/shared/navbar/TheNavigationDrawerChat'
 import PostDialog from '@/components/shared/view-post/PostDialog'
+import ChatContainer from '@/components/shared/chat/ChatContainer'
 
 export default {
   components: {
     TheNavbar,
     AppFooter,
     TheNavigationDrawerChat,
-    PostDialog
+    PostDialog,
+    ChatContainer
   },
   data() {
     return {}
