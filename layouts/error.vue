2f2124945115b48875b5bc2cf8491f594c3a315e
Marko Zlatar <markozlatar01@gmail.com>
2020-03-08 12:29:06 +0100

    CHANGED: Error page text display

--- layouts/error.vue
+++ layouts/error.vue
@@ -4,11 +4,14 @@
       <v-row>
         <v-col class="container d-flex flex-column justify-center align-center" cols="12">
           <v-icon size="100" class="error-icon">mdi-alert-octagon</v-icon>
-          <h1 class="text-center error-text" v-if="error.statusCode === 404">{{ pageNotFound }}</h1>
-          <h1 class="text-center error-text" v-else>
-            An Error Occurred
-            <br />Sorry for this inconvenience
-          </h1>
+          <h1 class="text-center text-404" v-if="error.statusCode === 404">{{ pageNotFound }}</h1>
+          <div v-else class="error-text-container d-flex flex-column align-center justify-center">
+            <h3 class="text-center error-text">
+              An Error Occurred
+              <br />
+            </h3>
+            <h4>Sorry for this inconvenience</h4>
+          </div>
           <a href="/" class="text-decoration-none mt-2">
             <v-btn>Back to home</v-btn>
           </a>
@@ -44,22 +47,37 @@ export default {
 </script>
 
 <style scoped lang="scss">
-h1 {
-  font-size: 20px;
-}
 .container {
   height: 95vh;
 }
-.error-text {
+.text-404 {
   font-size: 70px;
+}
+.error-text-container {
+  h3 {
+    font-size: 70px;
+    text-align: center;
+  }
+  h4 {
+    font-size: 35px;
+    text-align: center;
+  }
 }
 
 @media screen and (max-width: 600px) {
   .error-icon {
     font-size: 120px;
   }
-  .error-text {
+  .text-404 {
     font-size: 25px;
+  }
+  .error-text-container {
+    h3 {
+      font-size: 25px;
+    }
+    h4 {
+      font-size: 20px;
+    }
   }
 }
 </style>
