0f5ef5c380aefce97dbdd94666083dd43d9e1f3d
Marko Zlatar <markozlatar01@gmail.com>
2020-03-13 18:00:15 +0100

    CHANGED: Responsive design for profile posts

--- components/profile/ProfilePosts.vue
+++ components/profile/ProfilePosts.vue
@@ -16,7 +16,16 @@
       <v-tab-item>
         <v-col cols="12" sm="10" lg="9" xl="8" class="mx-auto d-flex flex-wrap">
           <h3 class="text-center mx-auto" v-if="!posts.length">User has no posts</h3>
-          <v-col v-else v-for="post in posts" :key="post._id" class="d-flex child-flex" cols="4">
+          <v-col
+            v-else
+            v-for="post in posts"
+            :key="post._id"
+            class="d-flex child-flex"
+            cols="12"
+            sm="6"
+            md="4"
+            xl="3"
+          >
             <profile-posts-post :post="post"></profile-posts-post>
           </v-col>
         </v-col>
